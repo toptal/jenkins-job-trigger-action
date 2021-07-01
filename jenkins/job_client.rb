aaca7693771ce7a264547a44666e59413fe2c315
Mateusz Derks <mateusz.derks@toptal.com>
2021-07-01 15:24:27 +0200

    Add more debug

--- jenkins/job_client.rb
+++ jenkins/job_client.rb
@@ -25,6 +25,8 @@ module Jenkins
       puts "::set-output name=jenkins_job_url::#{job_run_url}"
       puts "Job run URL: #{job_run_url}"
 
+      puts @async_mode
+      puts @async_mode.inspect
       exit(0) if @async_mode == true
 
       job_progress(job_run_url, job_timeout)
